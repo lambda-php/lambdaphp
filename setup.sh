b6b8307a3a65d984f5774107cb32ed74ab8ae5eb
evrobert <evrobert@users.noreply.github.com>
2021-01-10 22:26:51 -0500

    added /tmp/layer

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ echo ".......INSTALLLING PHP ........"
 sudo yum install php -y
 echo ".......FINSHED INSTALLLING PHP ........"
 
+echo ".......adding /tmp/layer ........"
+mkdir /tmp/layer
+cd /tmp/layer
+echo ".......FINSHED /tmp/layer ........"
+
 exit
