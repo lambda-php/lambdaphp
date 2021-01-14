728e059c5c83e75e40e9b5eb4d5299f3c901012f
evrobert <evrobert@users.noreply.github.com>
2021-01-14 15:36:06 -0500

    adding lib and file

--- setup.sh
+++ setup.sh
@@ -15,11 +15,20 @@ pwd
 ls -al
 echo ".......FINSHED /tmp/layer ........"
 
-echo ".......adding /bin/ and php ........"
+echo ".......adding /tmp/layer/bin/ and php ........"
 mkdir bin
 pwd
 cp /usr/bin/php bin/
 ls -al
-echo ".......FINSHED /bin/ and php ........"
+echo ".......FINSHED /tmp/layer/bin/ and php ........"
+
+echo ".......adding /tmp/layer/lib/ and moving files into the lib ........"
+mkdir lib
+pwd
+for lib in libncurses.so.5 libtinfo.so.5 libpcre.so.0; do
+  cp "/lib64/${lib}" lib/
+done
+ls -al
+echo ".......FINSHED /tmp/layer/lib/ and moving files into the lib ........"
 
 exit
