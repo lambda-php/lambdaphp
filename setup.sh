54bf76adc30d27938257257f8d43a4dd8a0f8192
evrobert <evrobert@users.noreply.github.com>
2021-01-14 16:24:55 -0500

    adding the corect way modules

--- setup.sh
+++ setup.sh
@@ -22,13 +22,18 @@ cp /usr/bin/php bin/
 ls -al
 echo ".......FINSHED /tmp/layer/bin/ and php ........"
 
-echo ".......adding /tmp/layer/lib/ and moving files into the lib ........"
+echo ".......adding /tmp/layer/lib/php/modules/ and moving files into the lib ........"
 mkdir lib
+cd lib
 pwd
-for lib in libncurses.so.5 libtinfo.so.5 libpcre.so.0; do
-  cp "/lib64/${lib}" lib/
-done
+mkdir php
+cd php
+pwd
+mkdir modules
+cd modules
+pwd
+cp /lib64/php/modules/*.* .
 ls -al
-echo ".......FINSHED /tmp/layer/lib/ and moving files into the lib ........"
+echo ".......FINSHED /tmp/layer/lib/php/modules/ and moving files into the lib ........"
 
 exit
